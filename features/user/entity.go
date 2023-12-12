a9bb6c2723723cb32144a6a7e3ee352efbbbbbec
Fauzi <brillianfauzi@gmail.com>
2023-12-12 23:49:02 +0700

    feat:register

--- features/user/entity.go
+++ features/user/entity.go
@@ -13,12 +13,15 @@ type User struct {
 
 type Handler interface {
 	Login() echo.HandlerFunc
+	Register() echo.HandlerFunc
 }
 
 type Service interface {
 	Login(email string, password string) (User, error)
+	Register(newUser User) (User, error)
 }
 
 type Repository interface {
+	InsertUser(newUser User) (User, error)
 	Login(email string) (User, error)
 }
--- features/user/handler/format.go
+++ features/user/handler/format.go
@@ -1,5 +1,19 @@
 package handler
 
+type UserRequest struct {
+	Email       string `json:"email" form:"email"`
+	Name        string `json:"name" form:"name"`
+	Address     string `json:"address" form:"address"`
+	PhoneNumber string `json:"phone_number" form:"phone_number"`
+	Password    string `json:"password" form:"password"`
+}
+
+type UserResponse struct {
+	ID    uint   `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 type LoginRequest struct {
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
