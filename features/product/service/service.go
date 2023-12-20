98b9155c5f50acd72e3d9cb997c4dac165cf94b2
John Matthew David <itsmejomada@gmail.com>
2023-12-20 13:39:46 +0700

    fix:migration template of chatgpt to helper & sorting transaction by desc

--- features/product/service/service.go
+++ features/product/service/service.go
@@ -1,16 +1,11 @@
 package service
 
 import (
-	"BE-hi-SPEC/config"
 	"BE-hi-SPEC/features/product"
 	"BE-hi-SPEC/helper/jwt"
-	"context"
-	"encoding/json"
 	"errors"
-	"fmt"
 
 	golangjwt "github.com/golang-jwt/jwt/v5"
-	openai "github.com/sashabaranov/go-openai"
 )
 
 type ProductServices struct {
@@ -57,29 +52,11 @@ func (ps *ProductServices) TalkToGpt(token *golangjwt.Token, newProduct product.
 	if err != nil {
 		return product.Product{}, err
 	}
-	client := openai.NewClient(config.InitConfig().OPEN_AI_KEY)
-	resp, err := client.CreateChatCompletion(
-		context.Background(),
-		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: "specification of" + newProduct.Name + "written as json following this specification {Name, CPU, RAM, Display, Storage, Thickness, Weight, Bluetooth(yes/no), HDMI(yes/no), Price(in Indonesia integer))} without any explanation",
-				},
-			},
-		},
-	)
+
 	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
 		return product.Product{}, err
 	}
 
-	err = json.Unmarshal([]byte(resp.Choices[0].Message.Content), &newProduct)
-	// fmt.Println(resp.Choices[0].Message.Content)
-	if err != nil {
-		return product.Product{}, err
-	}
 	result, err := ps.repo.InsertProduct(userId, newProduct)
 
 	return result, nil
