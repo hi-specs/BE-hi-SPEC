0e30ffd01ed42b8e8a2e14e52641c0b7631b3963
John Matthew David <itsmejomada@gmail.com>
2023-12-24 13:28:31 +0700

    fix: status code and massage

--- features/user/handler/user.go
+++ features/user/handler/user.go
@@ -174,12 +174,16 @@ func (uc *UserController) Update() echo.HandlerFunc {
 					var statusCode = http.StatusInternalServerError
 					var message = "terjadi permasalahan ketika memproses data"
 
+					if strings.Contains(err.Error(), "kesalahan pada database") {
+						statusCode = http.StatusNotFound
+						message = "User Tidak Terdaftar"
+					}
 					if strings.Contains(err.Error(), "id tidak cocok") {
-						statusCode = http.StatusBadRequest
+						statusCode = http.StatusUnauthorized
 						message = "Tidak Mempunyai Akses"
 					}
 					if strings.Contains(err.Error(), "terdaftar") {
-						statusCode = http.StatusBadRequest
+						statusCode = http.StatusConflict
 						message = "data yang diinputkan sudah terdaftar ada sistem"
 					}
 					if strings.Contains(err.Error(), "yang lama") {
